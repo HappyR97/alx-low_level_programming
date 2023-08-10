4e716fe70ad35f938bc59c6ec4f8ceb1197108d8
HappyR97 <ridaberkaoui1997@gmail.com>
2023-08-10 05:13:36 -0700

    Write 6-size.c program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point func
+ * Return: returns 0
+*/
+
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld\ byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+
+	return (0);
+}
