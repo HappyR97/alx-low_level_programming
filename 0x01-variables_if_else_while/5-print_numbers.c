36f7aaeeff805f375b1015865e75d988b6a28b17
HappyR97 <ridaberkaoui1997@gmail.com>
2023-08-11 04:42:51 -0700

    Write 5-print_numbers.c program

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
