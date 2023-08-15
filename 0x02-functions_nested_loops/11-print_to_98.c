8423315ed8e915b092cabec32ff36780accd0167
HappyR97 <ridaberkaoui1997@gmail.com>
2023-08-15 03:42:23 -0700

    Write 11-print_to_98.c program

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Prints digits until reaching 98
+ * @n: starting int
+ *
+ * Return: void
+*/
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d ,", n);
+			n++;
+		}
+		printf("98");
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d ,", n);
+			n--;
+		}
+		printf("98");
+		printf("\n");
+	}
+	else
+	{
+		printf("98");
+		printf("\n");
+	}
+}
