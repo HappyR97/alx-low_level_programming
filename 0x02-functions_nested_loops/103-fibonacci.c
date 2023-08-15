66d1c33b9944d809ae5d54011dcbc4365abd17fd
HappyR97 <ridaberkaoui1997@gmail.com>
2023-08-15 09:22:09 -0700

    Write 103-fibonacci.c program

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	unsigned long int f = 0;
+	unsigned long int s = 1;
+	unsigned long int t = 0;
+	unsigned long int sum = 0;
+
+	while (t < 4000000)
+	{
+		t = f + s;
+		f = s;
+		s = t;
+
+		if ((t % 2) == 0)
+		{
+			sum += t;
+		}
+	}
+	printf("%lu\n", sum);
+}
