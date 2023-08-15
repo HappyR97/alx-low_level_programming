ee2377099cf1845e4a40971e95d0fa20198edfac
HappyR97 <ridaberkaoui1997@gmail.com>
2023-08-15 05:09:14 -0700

    Write 102-fibonacci.c program

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int i = 0;
+	unsigned long int f = 0;
+	unsigned long int s = 1;
+	unsigned long int t;
+
+	while (i < 50)
+	{
+		t = f + s;
+		printf("%lu", t);
+
+		f = s;
+		s = t;
+		i++;
+
+		if (i == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
