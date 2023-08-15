e879ff3c27a9052abdfde037a884bf9904706bd8
HappyR97 <ridaberkaoui1997@gmail.com>
2023-08-15 13:12:54 -0700

    Hot fix 104-fibonacci.c program

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,26 +7,38 @@
 
 int main(void)
 {
-	int i = 0;
-	unsigned long int f = 0;
-	unsigned long int s = 1;
-	unsigned long int t;
+	unsigned long i;
+	unsigned long int b = 1;
+	unsigned long int a = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int b1;
+	unsigned long int b2;
+	unsigned long int a1;
+	unsigned long int a2;
 
-	while (i < 99)
+	printf("%lu", b);
+
+	for(i = 1; i < 91; i++)
 	{
-		t = f + s;
-		printf("%lu", t);
-		f = s;
-		s = t;
-		if (i == 98)
-		{
-			printf("\n");
-		}
-		else
-		{
-			printf(", ");
-		}
-		i++;
+		printf(", %lu", a);
+		a += b;
+		b = a - b;
 	}
+
+	b1 = b / l;
+	b2 = b % l;
+	a1 = a / l;
+	a2 = a % l;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", a1 + (a2 / l));
+		printf("%lu", a2 % l);
+		a1 = a1 + b1;
+		b1 = a1 - b1;
+		a2 = a2 + b2;
+		b2 = a2 - b2;
+	}
+	printf("\n");
 	return (0);
 }
