8fc4ca1678b7bc8b3ca2acc74a0c5f822d39dd44
HappyR97 <ridaberkaoui1997@gmail.com>
2023-08-16 01:21:16 -0700

    Creat directory for debugging, 0-main.c and main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	int i = 98;
+
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is the directory for 0x03. C - Debugging project
