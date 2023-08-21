67203eb01c01c2f81d414045c6274b4ebe3cad60
HappyR97 <ridaberkaoui1997@gmail.com>
2023-08-21 09:14:59 +0100

    Write 4-print_rev program

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: String to reverse
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
