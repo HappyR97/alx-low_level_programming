b35aaaf8cedcc7dfa438befe21e604bcd338751a
HappyR97 <ridaberkaoui1997@gmail.com>
2023-08-21 11:49:37 +0100

    Write 6-puts2.c program

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character, starting from first
+ * @str: string to use
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
