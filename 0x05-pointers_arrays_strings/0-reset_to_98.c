8333036ea7a74ad41a2d1c886af963568c935da9
HappyR97 <ridaberkaoui1997@gmail.com>
2023-08-21 08:25:23 +0100

    Fix 0-reset_to_98.c to be betty compliant

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98(int *n) - Resets n value to 98
+ * reset_to_98(int *n) - Resets n value to 98.
  * @n: Variable to reset
  *
- * Return: void
+ * This function takes a pointer to an integer as a parameter and sets the value
+ * of the integer to 98.
  */
 
 void reset_to_98(int *n)
