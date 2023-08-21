0ccd7ccadcc467fda1aed51167e2776628d29315
HappyR97 <ridaberkaoui1997@gmail.com>
2023-08-21 22:28:34 +0100

    Write 100-atoi.c program

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: String to be converted
+ *
+ * Return: The integer converted from the string
+ */
+int _atoi(char *s)
+{
+	 int i = 0;
+	 int num = 0;
+	 int str_length = 0;
+	 int negative = 0;
+	 int found_digit = 0;
+
+	while (s[str_length] != '\0')
+		str_length++;
+
+	while (i < str_length && found_digit == 0)
+	{
+		if (s[i] == '-')
+		{
+			++negative;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			int digit = s[i] - '0';
+
+			if (negative % 2)
+			{
+				digit = -digit;
+			}
+			num = num * 10 + digit;
+			found_digit = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				break;
+			}
+			found_digit = 0;
+		}
+		i++;
+	}
+
+	if (found_digit == 0)
+	return (0);
+
+	return (num);
+}
+
