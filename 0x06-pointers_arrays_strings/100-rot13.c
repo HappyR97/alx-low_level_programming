a05f6b1fc900c5e1901c97e28a6a8d6a0faa4373
HappyR97 <ridaberkaoui1997@gmail.com>
2023-08-24 12:56:15 +0100

    Write 100-rot13.c function

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *rot13 - Encodes a string using rot13
+ * @str: String to encode
+ * Return: New str (char)
+ */
+
+char *rot13(char *str)
+{
+	int i;
+	int j;
+	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		j = 0;
+
+		for (; input[j] != '\0'; j++)
+		{
+			if (str[i] == input[j])
+			{
+				str[i] = output[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
