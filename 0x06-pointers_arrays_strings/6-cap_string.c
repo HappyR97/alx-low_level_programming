8153e38c2596baaa44e546a38df93336bbb88e8d
HappyR97 <ridaberkaoui1997@gmail.com>
2023-08-24 10:31:12 +0100

    Write 6-cap_string.c function

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *cap_string - Capitalizes All Words Of A String
+ * @str: String To Capitalize
+ *
+ * Return: New Str
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < i; j++)
+	{
+		if (str[j] == ' ' || str[j] == '\t' || str[j] == '\n'
+			|| str[j] == ',' || str[j] == ';' || str[j] == '.'
+			|| str[j] == '!' || str[j] == '?' || str[j] == '"'
+			|| str[j] == ')' || str[j] == '(' || str[j] == '{'
+			|| str[j] == '}')
+		{
+			if (str[j + 1] > 96 && str[j + 1] < 123)
+			{
+				str[j + 1] = str[j + 1] - 32;
+			}
+		}
+	}
+	return (str);
+}
