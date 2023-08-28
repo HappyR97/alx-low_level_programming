2b0b712bd717446023471851c78eb403b669f70e
HappyR97 <ridaberkaoui1997@gmail.com>
2023-08-28 11:36:56 +0100

    Make 1-memcpy.c in betty style

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * *_memcpy - Copies memory area
+ * @dest: destination string
+ * @src: Source string
+ * @n: Number of bytes
+ * Return: dest (char)
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
