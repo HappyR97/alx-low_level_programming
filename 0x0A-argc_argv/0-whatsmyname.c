949c7d14e1f7d4e218453365a84f9d520986e0b8
HappyR97 <ridaberkaoui1997@gmail.com>
2023-09-01 17:26:39 +0100

    Write 0-whatsmyname.c program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point, prints program's name, followed by a new line
+ * @argc: Number of arguments
+ * @argv: array of argument strings
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is the directory for "0x0A. C - argc, argv" project in ALX
