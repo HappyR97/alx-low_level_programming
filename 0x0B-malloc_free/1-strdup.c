4554f5e33c0ed65c3f168ecae6c13480a22f1eee
HappyR97 <ridaberkaoui1997@gmail.com>
2023-09-04 17:39:56 +0100

    Write 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *_strdup - Returns pointer to a copy of another string
+ * @str: string to contain copy
+ *
+ * Return: Pointer to newly created string if success, 0 if not
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	int length = strlen(str);
+	char *p;
+
+	if (!str)
+		return (0);
+
+	p = malloc(length * sizeof(char));
+
+	if (!p)
+		return (0);
+
+	for (i = 0; i < length; i++)
+	{
+		*(p + i) = *(str + i);
+	}
+
+	return (p);
+}
