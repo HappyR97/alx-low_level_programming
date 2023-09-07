e333bd415b1adc1f78c777cbc4f830d9426d68c7
HappyR97 <ridaberkaoui1997@gmail.com>
2023-09-07 11:10:25 +0100

    Hot fix 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * *string_nconcat - Concatenates n bytes from second str to first str
@@ -13,10 +12,11 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int length_s1 = 0;
+	unsigned int length_s2 = 0;
 	char *concat;
-	unsigned int length_s1;
-	unsigned int length_s2;
 
 	if (!s1)
 		*s1 = '\0';
@@ -25,24 +25,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		*s2 = '\0';
 		n = 0;
 	}
-
-	length_s1 = strlen(s1);
-	length_s2 = strlen(s2);
-
+	while (s1[i] != '\0')
+	{
+		length_s1++;
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		length_s2++;
+		j++;
+	}
 	if (n >= length_s2)
 		n = length_s2;
-
-	concat = malloc(strlen(s1) + n + 1);
-
+	concat = malloc(length_s1 + n + 1);
 	if (!concat)
 		return (NULL);
-
 	for (i = 0; i < length_s1; i++)
 		*(concat + i) = *(s1 + i);
-
 	for (i = 0; i < n; i++)
 		*(concat + length_s1 + i) = *(s2 + i);
-
 	*(concat + length_s1 + i) = '\0';
 	return (concat);
 }
