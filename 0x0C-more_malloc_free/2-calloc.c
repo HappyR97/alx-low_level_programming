784e6a5f58458d0ad455c1e10afb5eefd7941727
HappyR97 <ridaberkaoui1997@gmail.com>
2023-09-07 12:01:51 +0100

    Fix 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (!arr)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		*(arr + i) = 0;
 	return (arr);
 }
