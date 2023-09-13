11dc9e1981c3f4b5c95274e5fe1cd675928bc9bc
HappyR97 <ridaberkaoui1997@gmail.com>
2023-09-13 23:20:17 +0100

    Correct all compilation errors and typos

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -12,20 +12,27 @@
 int (*get_op_func(char *s))(int, int)
 {
 	int i;
-	op_t operation[5];
+	op_t operations[5] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod}
+	};
 
+	/*
 	operations[0] = {'+', op_add};
 	operations[1] = {'-', op_sub};
 	operations[2] = {'*', op_mul};
 	operations[3] = {'/', op_div};
 	operations[4] = {'%', op_mod};
-
+	*/
 	i = 0;
 
 	while (i < 5)
 	{
 		if (s == operations[i].op)
-			return (operations[i].f)
+			return (operations[i].f);
 	}
 
 	return (NULL);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[2] == '/' || argv[2] == "%") && argv[0] == 0)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[0] == 0)
 	{
 		printf("Error\n");
 		exit(100);
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", operation((atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", operation(atoi(argv[1]), atoi(argv[3])));
 
 
 	return (0);
