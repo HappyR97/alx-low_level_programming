5fd8348e11a3ff95575f3f4d176ea069eb14f32d
HappyR97 <ridaberkaoui1997@gmail.com>
2023-09-13 22:48:56 +0100

    Write calc.h & op_functions.c

---
+++ 0x0F-function_pointers/op_functions.c
@@ -0,0 +1,66 @@
+#include "calc.h"
+
+/**
+ * op_add - Performs addition
+ * @a: first int to add
+ * @b: second int to add
+ *
+ * Return: a + b (int)
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Performs substraction
+ * @a: first int to substract
+ * @b: second int to substract
+ *
+ * Return: a - b (int)
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Performs multiplication
+ * @a: first int to multiply
+ * @b: second int to multiply
+ *
+ * Return: a * b (int)
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Performs division
+ * @a: first int to divide
+ * @b: second int to divide
+ *
+ * Return: a / b (int)
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Performs mod operation
+ * @a: first int to mod
+ * @b: second int to mod
+ *
+ * Return: a % b (int)
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
