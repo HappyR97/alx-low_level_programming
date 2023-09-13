e73202b6c0797c2d57c953a004598b2789812292
HappyR97 <ridaberkaoui1997@gmail.com>
2023-09-13 23:06:32 +0100

    Write 3-main.c & 3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_op_func - Gets the relevant function depending on operator
+ * @s: string operator
+ *
+ * Return: pointer to relevant function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+	op_t operation[5];
+
+	operations[0] = {'+', op_add};
+	operations[1] = {'-', op_sub};
+	operations[2] = {'*', op_mul};
+	operations[3] = {'/', op_div};
+	operations[4] = {'%', op_mod};
+
+	i = 0;
+
+	while (i < 5)
+	{
+		if (s == operations[i].op)
+			return (operations[i].f)
+	}
+
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point, performs calculation on command line args
+ * @argc: Number of arguments passed
+ * @argv: Array of arguments passed
+ * Return: 0 on success, 98 or 99 or 100 on failure.
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((argv[2] == '/' || argv[2] == "%") && argv[0] == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	operation = get_op_func(argv[2]);
+
+	if (!operation)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", operation((atoi(argv[1]), atoi(argv[3])));
+
+
+	return (0);
+}
