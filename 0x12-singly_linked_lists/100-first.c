fd260e814eea0b8f1f229a1ddc7109e7de3a3bf7
HappyR97 <ridaberkaoui1997@gmail.com>
2023-09-21 17:43:48 +0100

    Solve task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_quote - Constructor function that prints a quote
+ * 
+ * Return: void
+ */
+
+void print_quote(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
