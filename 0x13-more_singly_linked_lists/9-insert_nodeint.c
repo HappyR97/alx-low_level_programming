cd56b03d39684f11b896ec8f8e2b0fdb5f7889c2
HappyR97 <ridaberkaoui1997@gmail.com>
2023-09-26 12:29:04 +0100

    Minor fix task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	i = 0;
 	while (i < idx - 1)
 	{
-		if (!ptr)
+		if (ptr == NULL)
 		{
 			free(new_ptr);
 			return (NULL);
