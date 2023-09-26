f4ac32337b2cf7bec0185c933498a030078dd31b
HappyR97 <ridaberkaoui1997@gmail.com>
2023-09-26 12:06:03 +0100

    Hot fix task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,6 +27,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (i != idx - 1)
 	{
 		ptr = ptr->next;
+		if (!ptr)
+			return (NULL);
 		i++;
 	}
 	new_ptr->next = ptr->next;
