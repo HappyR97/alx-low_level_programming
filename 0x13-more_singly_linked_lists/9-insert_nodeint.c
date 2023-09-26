facea6cf968b268e747ed03d7e4cb12e4a874975
HappyR97 <ridaberkaoui1997@gmail.com>
2023-09-26 12:32:21 +0100

    Handle case of (head == NULL)

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_ptr = create_node(n);
 	count = listint_len(*head);
 
-	if (idx > count)
+	if (idx > count || head == NULL)
 		return (NULL);
 	if (idx == 0)
 	{
