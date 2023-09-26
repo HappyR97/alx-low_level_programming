6b1a27bf657e887b4b23c3ce29f632802a144e7c
HappyR97 <ridaberkaoui1997@gmail.com>
2023-09-26 12:15:53 +0100

    Improvement + handle case of idx = 0 for task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,25 +14,45 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr;
 	listint_t *new_ptr;
-	unsigned int i;
+	unsigned int i = 0;
 
-	new_ptr = malloc(sizeof(listint_t));
-	if (!new_ptr)
-		return (NULL);
-	new_ptr->n = n;
-	new_ptr->next = NULL;
-
-	ptr = *head;
-	i = 0;
-	while (i != idx - 1)
+	new_ptr = create_node(n);
+	if (idx == 0)
 	{
-		ptr = ptr->next;
+		new_ptr->next = *head;
+		*head = new_ptr;
+		return (new_ptr);
+	}
+	ptr = *head;
+	while (i < idx - 1)
+	{
 		if (!ptr)
+		{
+			free(new_ptr);
 			return (NULL);
+		}
+		ptr = ptr->next;
 		i++;
 	}
 	new_ptr->next = ptr->next;
 	ptr->next = new_ptr;
-
 	return (new_ptr);
+}
+
+/**
+ * *create_node - Creates a new listint_t node
+ * @n: New node's date
+ *
+ * Return: pointer to new node
+ */
+
+listint_t *create_node(int n)
+{
+	listint_t *ptr;
+
+	ptr = malloc(sizeof(listint_t));
+	ptr->n = n;
+	ptr->next = NULL;
+
+	return (ptr);
 }
