e6714b2253045a413bdaea53157136335d0ff34c
HappyR97 <ridaberkaoui1997@gmail.com>
2024-02-16 10:57:18 +0100

    Hot fix task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	unsigned long int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
@@ -23,9 +22,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(new_table);
 		return (NULL);
 	}
-
-	for (i = 0; i < size; i++)
-		new_table->array[i] = NULL;
-
 	return (new_table);
 }
