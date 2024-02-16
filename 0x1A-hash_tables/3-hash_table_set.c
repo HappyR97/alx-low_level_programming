d4c89e1b350895b43e161105217ab70e9d47c168
HappyR97 <ridaberkaoui1997@gmail.com>
2024-02-16 11:04:08 +0100

    Hot fix 2 task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node;
 
-	if (ht == NULL || *key == NULL || key == NULL || value == NULL)
+	if (!ht || !key || !*key || !value || !ht->array)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
